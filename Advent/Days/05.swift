d4db23a0f68ea6c304e2d6cdf53ff63ed5ac0362
Wilhelm Thieme <wjthieme@gmail.com>
2021-12-05 10:50:44 +0100

    Day 5

--- Advent/Days/05.swift
+++ Advent/Days/05.swift
@@ -7,12 +7,70 @@
 
 import Foundation
 
+fileprivate typealias Line = (x1: Int, y1: Int, x2: Int, y2: Int)
+fileprivate typealias Map = [[Int]]
+
 extension Solvers {
     @objc static let day05a: Solve = { input in
-        return ""
+        let lines = processInput(input).filter({ isHorizontal($0) || isVertical($0) })
+        let map = drawLines(lines)
+        let overlaps = countOverlaps(map)
+        return "\(overlaps)"
     }
     
     @objc static let day05b: Solve = { input in
-        return ""
+        let lines = processInput(input).filter({ isHorizontal($0) || isVertical($0) || isDiagonal($0) })
+        let map = drawLines(lines)
+        let overlaps = countOverlaps(map)
+        return "\(overlaps)"
     }
 }
+
+fileprivate func processInput(_ str: String) -> [Line] {
+    return str.split(separator: "\n").map({ processLine(String($0)) })
+}
+
+fileprivate func processLine(_ str: String) -> Line {
+    let splits = str.components(separatedBy: " -> ")
+    let first = splits[0].split(separator: ",").compactMap({ Int($0) })
+    let second = splits[1].split(separator: ",").compactMap({ Int($0) })
+    return (first[0], first[1], second[0], second[1])
+}
+
+fileprivate func isHorizontal(_ line: Line) -> Bool { line.y1 == line.y2 }
+fileprivate func isVertical(_ line: Line) -> Bool { line.x1 == line.x2 }
+fileprivate func isDiagonal(_ line: Line) -> Bool { abs(line.x1 - line.x2) == abs(line.y1 - line.y2) }
+
+fileprivate func drawLines(_ lines: [Line]) -> Map {
+    var map = Map(repeating: [Int](repeating: 0, count: 1000), count: 1000)
+    for line in lines {
+        if isHorizontal(line) {
+            for x in min(line.x1, line.x2)...max(line.x1, line.x2) {
+                map[line.y1][x] += 1
+            }
+        }
+        if isVertical(line) {
+            for y in min(line.y1, line.y2)...max(line.y1, line.y2) {
+                map[y][line.x1] += 1
+            }
+        }
+        if isDiagonal(line) {
+            let diff = abs(line.x1 - line.x2)
+            let xDir = line.x2 > line.x1 ? Int.addingReportingOverflow : Int.subtractingReportingOverflow
+            let yDir = line.y2 > line.y1 ? Int.addingReportingOverflow : Int.subtractingReportingOverflow
+            
+            for n in 0...diff {
+                let y = yDir(line.y1)(n).partialValue
+                let x = xDir(line.x1)(n).partialValue
+                map[y][x] += 1
+            }
+        }
+    }
+    return map
+}
+
+fileprivate func countOverlaps(_ map: Map) -> Int {
+    return map.flatMap({ $0 }).filter({ $0 > 1 }).count
+}
+
+
--- Tests/Days/05.swift
+++ Tests/Days/05.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 extension Inputs {
-    @objc static let day05a = ""
+    @objc static let day05a = "0,9 -> 5,9\n8,0 -> 0,8\n9,4 -> 3,4\n2,2 -> 2,1\n7,0 -> 7,4\n6,4 -> 2,0\n0,9 -> 2,9\n3,4 -> 1,4\n0,0 -> 8,8\n5,5 -> 8,2"
     @objc static let day05b = day05a
 }
 
 extension Outputs {
-    @objc static let day05a = "0"
-    @objc static let day05b = "0"
+    @objc static let day05a = "5"
+    @objc static let day05b = "12"
 }
